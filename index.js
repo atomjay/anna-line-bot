edb56c737d1590b287c984863c8674bc23323856
atomjay <ifatomjay@gmail.com>
2022-12-29 13:22:15 +0800

    fix index.js problems

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ function handleEvent(event) {
         url: "https://api.openai.com/v1/completions",
         headers: {
           "Content-Type": "application/json",
-          Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+          "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`,
         },
         json: {
           model: "text-davinci-003",
